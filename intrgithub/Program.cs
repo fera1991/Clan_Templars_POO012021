87ee4c440be3cfe18a2fadb6595f84d0649d1b5f
fera1991 <00120520@uca.edu.sv>
2021-03-11 10:18:48 -0600

    add: intrgithub

---
+++ intrgithub/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace intrgithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Enter a value for a :  ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b :  ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {addnumbers(a,b)} ");
+        }
+
+        public static int addnumbers(int a,int b)
+        {
+            return a+b;
+        }
+
+        
+        
+    }
+}
+
+
